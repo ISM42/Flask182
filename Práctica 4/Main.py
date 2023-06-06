d58ea5e7c3a2d8958500969fa4c1bd404ef8b4f3
ISM42 <133287937+ISM42@users.noreply.github.com>
2023-06-06 09:34:47 -0600

    Práctica 4_versión2
    Hace falta que el programa elimine correctamente un registro.

--- Práctica 4/Main.py
+++ Práctica 4/Main.py
@@ -1,85 +1,141 @@
-listaproductos=[]
+listaproductos=[] 
 class almacenBebidas(object): 
-    def __init__(self,_id,_nombre,_precio,_clasificacion,_marca): 
-        self.id = _id
+    def __init__(self,_id, _nombre, _clasificacion, _marca, _precio): 
+        self.id =_id
         self.nombre = _nombre
-        self.precio = _precio
         self.clasificacion = _clasificacion
         self.marca = _marca
+        self.precio= _precio
+        self.historial = []
         
-    def DarAlta(self): 
-        print("ID: {} -{} -{} -{} -{}".format(self.id,self.nombre,self.precio,self.clasificacion,self.marca))
-        
-    def DarBaja(self,_id): 
-        self.id = _id
-        print("Producto eliminado exitosamente")
+    def entregarInformacion(self):
+        print("ID: {} - Nombre : {} - Clasificación:  {} - Marca:  {} - Precio : {}".format(self.id, self.nombre, self.clasificacion, self.marca,self.precio))
 
-    
-    def Actualizar(self,_id,_nombre,_precio,_clasificacion,_marca):
+    def editarRegistro(self,_id, _nombre, _clasificacion, _marca, _precio): 
         self.id = _id
         self.nombre = _nombre
-        self.precio = _precio
         self.clasificacion = _clasificacion
         self.marca = _marca
-        print("Informacion actualizada correctamente")
-
+        self.precio= _precio
+        
+        print("Actualizacion realizada")
+    def incluirEvento(self,_id, _nombre, _clasificacion, _marca, _precio): 
+      
+        return("Modificacion: ID: {} - Nombre: {} - Clasificación: {} - Marca: {} - Precio {}".format(_id,_nombre, _clasificacion, _marca, _precio))
+    def entregaHistorial(self):
+        print("ID: {} - Nombre: {} - Clasificación: {} - Marca: {} - Precio {}".format(self.id, self.nombre, self.clasificacion, self.marca, self.precio))
 def registrarProducto():
-        id = int(input("Ingresa el ID: ")) 
-        nombre = str(input("Ingresa nombre del producto: "))
-        precio = float(input("Ingresa el precio del producto: "))
-        clasificacion = str(input("Ingresa la clasificacion del producto: "))
+        id = int(input("Ingresa el ID del producto: "))
+        nombre = str(input("Ingresa el nombre del producto: ")) 
+        clasificacion = str (input("Ingresa la clasificación del producto: "))
         marca = str(input("Ingresa la marca del producto: "))
-    
-        objBebida = almacenBebidas(id, nombre,precio, clasificacion, marca)
+        precio = float(input("Ingresa el precio del producto: "))
+        objBebida = almacenBebidas(id, nombre, clasificacion, marca, precio)
         listaproductos.append(objBebida) 
-
 def mostraralmacenBebidas(): 
         print("Lista de productos")
         for objBebida in listaproductos: 
             objBebida.entregarInformacion() 
-
-"""def mostrar():
-    print("Mostrar informacion")
+def buscaralmacenBebidas(): 
+    print("Buscar productos")
+    id = int(input("Ingresa el ID del producto a buscar:")) 
     for objBebida in listaproductos: 
-        objBebida.DarBaja()"""
-    
-def PrecioPromedio():
-    print("Calcular precio promedio de bebidas")
-    resultado = float(_precio)
-    print("Precio promedio de bebida".format(resultado))
-    
-def BebidasMarca():
-    print("Cantidad de bebidas de una marca")
-    
-def Clasificacion():
-    print("cantidad por clasificacion")
+        if id ==objBebida.id:
+            objBebida.entregarInformacion() 
+def modificarBebida():
+    print("Modificar bebida")
+    id = int(input("Ingresa el ID del producto a modificar: "))
+    for objBebida in listaproductos: 
+        if id == objBebida.id:  
+            id = int(input("Ingresa el nuevo ID: ")) 
+            nombre = str(input("Ingresa el nuevo nombre: ")) 
+            clasificacion = str(input("Ingresa la nueva clasificación :")) 
+            marca = str(input("Ingresa la nueva marca del producto: "))
+            precio = float(input("Ingresa el nuevo precio del producto: "))
+            objBebida.editarRegistro(id,nombre,clasificacion,marca,precio)
+            objBebida.entregarInformacion()
+            recepcionMensaje = objBebida.incluirEvento(id,nombre,clasificacion,marca,precio)
+            objBebida.historial.append(recepcionMensaje)
+def consultaHistorial():
+    print("Consulta de historial del producto")
+    id = int(input("Ingresa el ID del producto a consultar: "))
+    for objBebida in listaproductos: 
+        if id == objBebida.id: 
+            for recepcionMensaje in objBebida.historial:
+                print("Evento : {}".format(recepcionMensaje)) 
 
-    def salir():
+def eliminarRegistro():
+    id= print(input("Ingrese el ID del producto que desea eliminar: "))
+    bebida=None 
+
+    for objBebida in listaproductos:
+        if objBebida.id == id:
+            bebida=objBebida
+            break
+        if bebida:
+            listaproductos.remove(bebida)
+            print("Registro eliminado correctamente")
+
+        else:
+            print("No se encontró el registro ingresado.")
+
+
+def promedioBebida():
+    total=sum(almacenBebidas.precio for almacenBebidas in listaproductos)
+    promedio=total/len(listaproductos)
+    print(f"El precio promedio de las bebidas es: {promedio}")
+
+def cantClasificacion():
+    clasificacion = input("Ingrese la clasificación de la que desea obtener el conteo: ")
+    cantidad = sum(1 for almacenBebidas in listaproductos if almacenBebidas.clasificacion == clasificacion)
+    print(f"La cantidad de bebidas de la clasificación solicitada es:  {cantidad}")
+
+def cantMarca():
+     marca=input("Ingrese el nombre de la marca que desea conocer: ")
+     cantidad = sum (1 for almacenBebidas in listaproductos if almacenBebidas.marca == marca)
+     print(f"La cantidad de bebidas de la marca solicitada es: {cantidad}")  
+
+def salir():
     print("Salir del programa")
     exit()
 
 
-def main():  
+def main():  #main es el metodo principal donde uniremos todos los metodos para que realice diferentes funciones
         while True:
             print("             MENU          ")
-            print("1.- Dar de alta un producto")
-            print("2.- Dar de baja un producto")
-            print("3.- Mostrar productos")
-            print("4.- Actualizar producto")
-            print("5.- Salir")
-         
+            print("1.- Registrar un producto nuevo")
+            print("2.- Mostrar lista de productos")
+            print("3.- Buscar producto")
+            print("4.- Editar registro")
+            print("5.- Precio promedio por bebida")
+            print("6.- Cantidad de bebidas por clasificación")
+            print("7.- Cantidad de bebidas por marca")
+            print("8.- Eliminar registro")
+            print("9.- Mostrar Historial de producto")
+            print(" PRESIONE 0 PARA SALIR")
+           
             opcion = int(input("Opcion: "))
 
             if opcion == 1:
-                DarAlta()
+                registrarProducto()
             elif opcion == 2:
-                DarBaja()
-            elif opcion == 3:
                 mostraralmacenBebidas()
+            elif opcion == 3:
+                buscaralmacenBebidas()
             elif opcion == 4:
-                Actualizar()
+                modificarBebida()
             elif opcion == 5:
+                promedioBebida()
+            elif opcion == 6:
+                cantClasificacion()
+            elif opcion == 7:
+                cantMarca()    
+            elif opcion == 8:
+                eliminarRegistro()            
+            elif opcion == 9:
+                consultaHistorial()
+            elif opcion == 0:
                 salir()
-        
+
 if __name__ == '__main__':
     main()
