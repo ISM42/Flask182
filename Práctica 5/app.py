9e912e64adb319c76c1f04ce9c3db80bbb6b96c4
ISM42 <133287937+ISM42@users.noreply.github.com>
2023-06-16 18:49:29 -0600

    Práctica 6: vistas, información y envío a rutas

--- Práctica 5/app.py
+++ Práctica 5/app.py
@@ -1,6 +1,7 @@
 #El app.py va a ser donde se hacen todas las configuraciones para el servidor de las peticiones que se harán a través del navegador
-
-from flask import Flask #importación de la librería
+#el render template es una librería que nos permite mostrar la vista a través de la ruta especificada
+#request es una librería para solicitudes
+from flask import Flask,render_template,request #importación de la librería
 
 app= Flask(__name__) #se declara el app y se inicializa el servidor Flask
 app.config['MYSQL_HOST']= "localhost" #aquí tmb se hacen las configuraciones a la conexión al servidor de la BD
@@ -14,11 +15,17 @@ app.config['MYSQL_DB']= "bdflask"
 #la ruta se compone de nombre y la función que va ejecutar
 @app.route('/') ##declarar la ruta y el nombre. La diagonal es por default. Es la primera ruta que el servidor va a buscar
 def index():
-    return "Hola Mundo"
+    return render_template('index.html')
 #cada ruta lleva su ruta y su función.
-@app.route('/guardar') ##Una segunda ruta o más ya pueden tener otros nombres.
-def guardar():
-    return "Se guardo el album en la BD"
+#con el método, la ruta va a atrapar la información por método POST, las rutas que no declaren el método usarán GET de forma predeterminada.
+@app.route('/guardar',methods=['POST']) ##Una segunda ruta o más ya pueden tener otros nombres.
+def guardar(): 
+    if request.method == 'POST':
+        titulo=request.form['txtTitulo']
+        artista=request.form['txtArtista']
+        anio=request.form['txtAnio']
+        print(titulo,artista,anio)
+    return "La información del álbum llegó a su ruta amigo ;)"
 
 @app.route('/eliminar') 
 def eliminar():
