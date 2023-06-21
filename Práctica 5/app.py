0607c9f3c6541a6a99a80c3c65b4cc8a91b3ec21
ISM42 <133287937+ISM42@users.noreply.github.com>
2023-06-20 18:56:42 -0600

    Finalización práctica 6

--- Práctica 5/app.py
+++ Práctica 5/app.py
@@ -1,14 +1,18 @@
 #El app.py va a ser donde se hacen todas las configuraciones para el servidor de las peticiones que se harán a través del navegador
 #el render template es una librería que nos permite mostrar la vista a través de la ruta especificada
 #request es una librería para solicitudes
-from flask import Flask,render_template,request #importación de la librería
+#redirect y url_for nos sirve para redireccionar a nuestro formulario terminado de llenar una vez
+from flask import Flask,render_template,request,redirect,url_for, flash #importación de la librería
+from flask_mysqldb import MySQL
 
 app= Flask(__name__) #se declara el app y se inicializa el servidor Flask
 app.config['MYSQL_HOST']= "localhost" #aquí tmb se hacen las configuraciones a la conexión al servidor de la BD
 app.config['MYSQL_USER']= "root"
 app.config['MYSQL_PASSWORD']= ""
-app.config['MYSQL_DB']= "bdflask"
+app.config['MYSQL_DB']= "dbflask"
 
+app.secret_key='mysecretkey'
+mysql= MySQL(app) 
 #declaración de rutas
 
 #ruta index o ruta principal http://localhost:5000 o 127.0.0.1 en lugar de localhost
@@ -21,11 +25,17 @@ def index():
 @app.route('/guardar',methods=['POST']) ##Una segunda ruta o más ya pueden tener otros nombres.
 def guardar(): 
     if request.method == 'POST':
-        titulo=request.form['txtTitulo']
-        artista=request.form['txtArtista']
-        anio=request.form['txtAnio']
-        print(titulo,artista,anio)
-    return "La información del álbum llegó a su ruta amigo ;)"
+        Vtitulo=request.form['txtTitulo']
+        Vartista=request.form['txtArtista']
+        Vanio=request.form['txtAnio']
+        #print(titulo,artista,anio)
+        CS=mysql.connection.cursor()
+        CS.execute('insert into albums(titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+        mysql.connection.commit()
+        
+    flash('Album agregado correctamente')
+    return redirect(url_for('index'))
+     
 
 @app.route('/eliminar') 
 def eliminar():
