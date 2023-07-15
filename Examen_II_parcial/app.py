dccab2f10e62c1b4ebe08393ef9616ae66c94bb1
ISM42 <133287937+ISM42@users.noreply.github.com>
2023-07-14 18:11:17 -0600

    Creación del app, vistas index, consulta, eliminar y actualizar no funcionales

---
+++ Examen_II_parcial/app.py
@@ -0,0 +1,92 @@
+from flask import Flask,render_template,request,redirect,url_for, flash 
+from flask_mysqldb import MySQL
+
+app= Flask(__name__) 
+app.config['MYSQL_HOST']= "localhost" 
+app.config['MYSQL_USER']= "root"
+app.config['MYSQL_PASSWORD']= ""
+app.config['MYSQL_DB']= "db_floreria"
+
+app.secret_key='mysecretkey'
+mysql= MySQL(app) 
+
+@app.route('/') 
+def index():
+    index=mysql.connection.cursor()
+    index.execute('select * from tbflores')
+    consulta=index.fetchall() 
+   
+    return render_template('index.html',listaFlores=consulta)
+
+@app.route('/guardar',methods=['POST']) 
+def guardar(): 
+    if request.method == 'POST':
+       _nombre=request.form['txtNombre']
+       _cantidad=request.form['txtCantidad']
+       _precio=request.form['txtPrecio']
+        
+       guardar=mysql.connection.cursor()
+       guardar.execute('insert into tbflores(nombre,cantidad,precio) values(%s,%s,%s)',(_nombre,_cantidad,_precio))
+       mysql.connection.commit()
+        
+    flash('Producto agregado correctamente')
+    return redirect(url_for('index'))
+
+
+@app.route('/vinventario') 
+def inventario():
+    inv=mysql.connection.cursor()
+    inv.execute('select * from tbflores')
+    mysql.connection.commit()
+
+    consulta_inv=inv.fetchall() 
+    return render_template('inventario.html',listaFlores=consulta_inv)
+
+    return render_template('consulta.html')     
+
+    
+@app.route('/vactualizar/<id>') 
+def editar(id):
+    editar=mysql.connection.cursor()
+    editar.execute('select * from tbflores where id = %s',(id,))
+    consulta=editar.fetchone() 
+    return render_template('actualizar_producto.html',flores=consulta)
+
+
+@app.route('/actualizar/<id>',methods=['POST']) 
+def actualizar(id): 
+   if request.method == 'POST':
+       _nombre=request.form['txtNombre']
+       _cantidad=request.form['txtCantidad']
+       _precio=request.form['txtPrecio']
+
+       curAct=mysql.connection.cursor()
+       curAct.execute('update tbflores set nombre=%s, cantidad=%s, precio=%s where id = %s', (_nombre,_cantidad,_precio))
+       mysql.connection.commit()
+
+       flash('Producto actualizado en la base de datos correctamente')
+       return redirect(url_for('index'))
+   
+@app.route('/veliminar') 
+def eliminaralbum():
+   
+    return render_template('eliminar_producto.html')
+ 
+
+
+@app.route('/eliminar/<id>',methods=['POST']) 
+def eliminar(id):
+    if request.method=='POST':
+      eliminar=mysql.connection.cursor()
+      eliminar.execute('delete from tbflores where id = %s',(id,))
+      mysql.connection.commit()
+
+    
+    flash('Producto eliminado correctamente de la BD')
+    return redirect(url_for('index'))
+
+
+
+if __name__== '__main__':
+    app.run(port=5000,debug=True) 
+    
